c201cf33cb76b0bd547607205d95d5e7d9039e2c
JooJ <joaopedroacostaalvarez@gmail.com>
2026-01-28 19:54:29 -0300

    TaskService refatory and more readble

--- src/main/java/br/com/todolist/toDoList/services/TaskService.java
+++ src/main/java/br/com/todolist/toDoList/services/TaskService.java
@@ -3,6 +3,7 @@ package br.com.todolist.toDoList.services;
 import br.com.todolist.toDoList.dtos.task.TaskCreateDTO;
 import br.com.todolist.toDoList.dtos.task.TaskResponseDTO;
 import br.com.todolist.toDoList.dtos.task.TaskUpdateDTO;
+import br.com.todolist.toDoList.enums.Status;
 import br.com.todolist.toDoList.repository.TaskRepository;
 import br.com.todolist.toDoList.entities.TaskEntity;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,8 +23,14 @@ public class TaskService {
         validateDatesForCreate(taskDTO.startAt(), taskDTO.endAt());
 
         TaskEntity task = new TaskEntity();
-        task.setTitle(taskDTO.title());
-        task.setDescription(taskDTO.description());
+        if (task.getTitle() != null){
+            task.setTitle(taskDTO.title());
+        }
+
+        if (task.getDescription() != null){
+            task.setDescription(taskDTO.description());
+        }
+        task.setStatus(Status.PENDING);
         task.setStartAt(taskDTO.startAt());
         task.setEndAt(taskDTO.endAt());
         task.setIdUser(userId);
@@ -38,11 +45,19 @@ public class TaskService {
 
     public TaskResponseDTO updateTask(Long taskId, Long userId, TaskUpdateDTO taskDTO) {
         TaskEntity task = taskRepository.findByIdAndIdUser(taskId,userId).
-                orElseThrow(() -> new IllegalArgumentException("Tarefa não encontrada"));
+                orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.BAD_REQUEST,
+                        "A data de término deve ser depois da data de início"));
 
         validateDatesForUpdate(taskDTO.startAt(), taskDTO.endAt());
-        task.setTitle(taskDTO.title());
-        task.setDescription(taskDTO.description());
+        if (task.getTitle() != null){
+            task.setTitle(taskDTO.title());
+        }
+
+        if (task.getDescription() != null){
+            task.setDescription(taskDTO.description());
+        }
+        task.setStatus(taskDTO.status());
         task.setStartAt(taskDTO.startAt());
         task.setEndAt(taskDTO.endAt());
 
@@ -51,23 +66,44 @@ public class TaskService {
     public void deleteTask(Long taskId, Long userId) {
 
         TaskEntity task = taskRepository.findByIdAndIdUser(taskId,userId).
-                orElseThrow(() -> new IllegalArgumentException("Tarefa não encontrada"));
-
-        if (!task.getIdUser().equals(userId)) {
-            throw new ResponseStatusException(
-                    HttpStatus.NOT_FOUND, "Tarefa não encontrada");
-        }
+                orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.BAD_REQUEST,
+                        "A data de término deve ser depois da data de início"));
 
         taskRepository.delete(task);
     }
     private void validateDatesForCreate(LocalDateTime startAt, LocalDateTime endAt) {
+        validateCommonDates(startAt, endAt);
+        validateDatesNotInPast(startAt, endAt);
+    }
 
+    private void validateDatesForUpdate(LocalDateTime startAt, LocalDateTime endAt) {
+        validateCommonDates(startAt,endAt);
+    }
+    private TaskResponseDTO mapToResponse(TaskEntity task) {
+        return new TaskResponseDTO(
+                task.getId(),
+                task.getTitle(),
+                task.getDescription(),
+                task.getStartAt(),
+                task.getEndAt(),
+                task.getStatus()
+        );
+    }
+    private void validateCommonDates(LocalDateTime startAt, LocalDateTime endAt) {
         if (startAt == null || endAt == null) {
             throw new ResponseStatusException(
                     HttpStatus.BAD_REQUEST,
                     "Datas de início e término são obrigatórias");
         }
 
+        if (endAt.isBefore(startAt)) {
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST,
+                    "A data de término deve ser depois da data de início");
+        }
+    }
+    private void validateDatesNotInPast(LocalDateTime startAt, LocalDateTime endAt) {
         var now = LocalDateTime.now();
 
         if (startAt.isBefore(now)) {
@@ -81,42 +117,8 @@ public class TaskService {
                     HttpStatus.BAD_REQUEST,
                     "A data de término não pode ser no passado");
         }
-
-        if (endAt.isBefore(startAt)) {
-            throw new ResponseStatusException(
-                    HttpStatus.BAD_REQUEST,
-                    "A data de término deve ser depois da data de início");
-        }
     }
 
-    private void validateDatesForUpdate(LocalDateTime startAt, LocalDateTime endAt) {
-
-        var now = LocalDateTime.now();
-
-        if (startAt == null || endAt == null) {
-            throw new ResponseStatusException(
-                    HttpStatus.BAD_REQUEST,"Datas obrigatórias");
-        }
-
-        if (endAt.isBefore(startAt)) {
-            throw new ResponseStatusException(
-                    HttpStatus.BAD_REQUEST,"Fim deve ser depois do início");
-        }
-        if (startAt.isBefore(now)) {
-            throw new ResponseStatusException(
-                    HttpStatus.BAD_REQUEST,
-                    "A data de início não pode ser no passado");
-        }
-
-    }private TaskResponseDTO mapToResponse(TaskEntity task) {
-        return new TaskResponseDTO(
-                task.getId(),
-                task.getTitle(),
-                task.getDescription(),
-                task.getStartAt(),
-                task.getEndAt()
-        );
-    }
 
 
 
